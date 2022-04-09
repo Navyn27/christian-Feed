d8c93b8d715230fea079b9aa1d97828cb60a160f
Yvan Sugira <phoenixnavyn7@gmail.com>
2022-04-09 23:43:48 +0300

    Making changes to Christian Feed

--- scripts/back.js
+++ scripts/back.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 const app = express();
-const Subscriber = require("./models.js");
+const { Subscriber, Sermon, Convos } = require("./models.js");
 
 // const connString =
 //   "mongodb+srv://sugirayvan:GhostOfYvan@dbghost.igi9x.mongodb.net/dbGhost?retryWrites=true&w=majority";
@@ -31,16 +31,85 @@ app.set("views", "../views");
 app.use(express.static("../Public"));
 
 app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
 app.get("/sermons", (req, res) => {
-  res.send("Sermons");
+  Sermon.find()
+    .then((results) => {
+      res.render("sermons", { title: "Sermon", data: results });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.get("/sermon", (req, res) => {
+  Sermon.find(req.body.sermonTitle)
+    .then((results) => {
+      res.render("sermon", { title: "Sermon", data: results });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.get("/newSermon", (req, res) => {
+  res.render("newSermon", { title: "New Sermon" });
 });
 
 app.get("/convos", (req, res) => {
-  res.send("convos");
+  Convos.find()
+    .then((results) => {
+      res.render("convos", { title: "Conversation", data: results });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.get("/convo", (req, res) => {
+  res.render("convo", { title: "Conversation" });
+});
+
+app.get("/newConvo", (req, res) => {
+  res.render("newconvo", { title: "New Conversation" });
+});
+
+app.post("/newConvo", (req, res) => {
+  const conversation = new Convos({
+    title: req.body.title,
+    email: req.body.email,
+    author: req.body.author,
+    content: req.body.content,
+  });
+  conversation
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post("/newSermon", (req, res) => {
+  const newSermon = new Sermon({
+    title: req.body.title,
+    email: req.body.email,
+    author: req.body.author,
+    content: req.body.content,
+  });
+  newSermon
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 });
 
 app.post("/intensive/subscribe", (req, res) => {
@@ -58,6 +127,7 @@ app.post("/intensive/subscribe", (req, res) => {
       console.log(Subscriber);
     });
 });
+
 app.use((req, res) => {
   res.send("404");
 });
--- scripts/models.js
+++ scripts/models.js
@@ -7,7 +7,32 @@ const emailSchema = new Schema({
     required: true,
   },
 });
+const sermonSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
+const Sermon = mongoose.model("sermon", sermonSchema);
+const Convos = mongoose.model("convo", sermonSchema);
 const Subscriber = mongoose.model("Cfsubscriber", emailSchema);
 
-module.exports = Subscriber;
+module.exports = { Subscriber, Sermon, Convos };
